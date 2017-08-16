35e1cfae66846933b796d27048c3f9959ef54fa8
ivan <ivangbodnar@gmail.com>
2017-08-16 13:53:57 -0300

    Modificado prueba con server

--- prueba.py
+++ prueba.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from database.db import DbConnection
-from database.conexiones import pfa_dgc_local
+from database.conexiones import pfa_dgc_server
 
-conn = DbConnection(pfa_dgc_local)
+conn = DbConnection(pfa_dgc_server)
 engine = conn.get_engine()
 print(pd.read_sql('select * from hechos limit 10;', engine))
